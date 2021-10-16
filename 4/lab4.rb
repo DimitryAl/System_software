c3d59db6e9945ab713712acb457eec3e493ffde7
Dmitry <mr.dima.alexeev@gmail.com>
2021-10-16 20:31:59 +0300

    Add files via upload

--- 4/lab4.rb
+++ 4/lab4.rb
@@ -1,44 +1,58 @@
 require_relative "semaphore"
 require 'thread'
 
+def generateNumber(x)
+    t = rand($semaphore_array.length)
+    while t == x
+        t = rand($semaphore_array.length)
+    end
+    return t
+end
+
 class Interface 
     attr_accessor :name, 
-                  :phone
+                  :phone,
+                  :number
 
-    def Wait()
-        sleep(2)
+    def Wait(x)
+        sleep(x)
     end
 
-    def MakeCall    #сделать звонок
-        # обращаемся к случайному элементу массива, если count == 0, то false,  иначе true        
-        n = rand($threads.length)
-        test =  $threads[n]
-        puts test
-        return true
-        # if $threads[n].phone.count == 0
-            
-        # end
-
+    def MakeCall(x)    #сделать звонок
+        n = generateNumber(x)
+        res = $semaphore_array[n]
+        puts @name + ' is calling to ' + $names[n]
+        if res.count == 0
+            res.up!(1)
+            @phone.up!
+            return true, n
+        else
+            return false, -1
+        end
     end
 
 end
 
 class Polyeuctus < Interface
 
-    def initialize(name, phone)
+    def initialize(name, phone, number)
         @name = name
         @phone = phone
-        #puts 'This is ' + @name
-                        
+        @number = number
+
         while true  #Пытается дозвониться до кого-то и если успешно, то перестает
-            puts @name + ' trying to call someone'
-            if MakeCall()   
-                puts @name + 'called to '
-                sleep(2)
+            while @phone.count != 0
+                Wait(1)
+            end
+            test = MakeCall(@number)   
+            if test[0] == true
+                puts @name + ' successfully called to ' + $names[test[1]]
+                Wait(2)
+                $semaphore_array[test[1]].down!(1)
                 break
             else
-                puts 'Call failed, ' + name + ' sleeps for 2'
-                Wait()
+                puts 'Call failed, ' + @name + ' waits'
+                Wait(2)
             end
         end    
     end
@@ -47,10 +61,11 @@ end
 
 class Mother < Interface
     
-    def initialize(name, phone)
+    def initialize(name, phone, number)
         @name = name
         @phone = phone 
-        
+        @number = number
+
         #puts 'This is ' + @name
 
     end
@@ -90,22 +105,23 @@ n = 2                   # кол-во семафоров
 girlfriend_number = 2   # кол-во девушек
 granny_number = 3       # кол-во бабушек
 
-semaphore_array = []
+$names = []*n
+$names[0] = 'Polyectus'
+$names[1] = 'Mother'
+$semaphore_array = []*n
 for i in (1..n)
-    semaphore_array << Semaphore.new(1)
+    $semaphore_array << Semaphore.new(1) 
 end
 
 $threads = []
-$threads << Thread.new {pol = Polyeuctus.new('Polyeuctus', semaphore_array[0])}
-$threads << Thread.new {mom = Mother.new('Mother', semaphore_array[1])}
+$threads << Thread.new {pol = Polyeuctus.new('Polyeuctus', $semaphore_array[0], 0)}
+$threads << Thread.new {mom = Mother.new('Mother', $semaphore_array[1], 1)}
 # for i in (1..girlfriend_number)
 #     threads << Thread.new {girl = Girlfriend.new('Girlfriend' + i.to_s)}
 # end
 # for i in (1..granny_number)
 #     threads << Thread.new {granny = Grandmother.new('Granny' + i.to_s)}
 # end
-
-
 
 
 
